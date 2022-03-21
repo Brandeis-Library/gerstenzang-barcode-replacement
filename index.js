df04c078250b12b160cafe6319e0c9b40beb130c
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-03-21 15:39:21 -0700

    gained access to the data in the spreadsheet and converted it to json.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var XLSX = require("xlsx");
 
 (async function () {
+  const workbook = XLSX.readFile("TestData1.xlsx");
+  //console.log("workbook ---------- ", workbook);
+  const sheetNames = workbook.SheetNames;
+  const sheetIndex = 1;
+
+  var df = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[sheetIndex - 1]]);
+  console.log("df--------------- ", df);
   console.log("Can you see me now?");
 })();
