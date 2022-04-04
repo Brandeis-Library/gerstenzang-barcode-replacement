8a842792f11421bfa632adeb329d28ebc55c25b0
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-04-04 16:32:43 -0700

    moved error repoting up a level and started the process of seperating the 2 async calls into different loops.

--- index.js
+++ index.js
@@ -17,81 +17,76 @@ const axios = require("axios");
   // Write at the top of the page.
   await fs
     .createWriteStream("./errorLog.csv", { flags: "a" })
-    .write(`Barcode, Error Message, Error Block, Path URL, \n`);
+    .write(`Barcode, Error Message, Error Block, \n`);
 
-  const workbook = XLSX.readFile("TestData3.csv");
+  const workbook = await XLSX.readFile("TestData3.csv");
   //console.log("workbook ---------- ", workbook);
   const sheetNames = workbook.SheetNames;
   const sheetIndex = 1;
 
-  var df = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[sheetIndex - 1]]);
+  var df = await XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[sheetIndex - 1]]);
   //console.log("df--------------- ", df);
   try {
-    await df.forEach(async (item, index, df) => {
+    const dataObjs = await df.forEach(async (item, index, df) => {
       //item retrieve query to Alma backend. API URL, Item Barcode, and APIKEY
 
       //const barcode = req.body.barcode.text;
       //console.log("barcode: ", item.Barcode);
-      try {
-        const { data } = await axios.get(
-          process.env.DEV_EXLIBRIS_API_ROOT +
-            process.env.DEV_EXLIBRIS_API_PATH +
-            item.Barcode +
-            //item["Scanned Barcode"] +
-            "&apikey=" +
-            process.env.DEV_EXLIBRIS_API_BIB_GET_KEY +
-            "&expand=p_avail",
-        );
-        //console.log("data -----   ", data);
-        //item update query to Alma backend. API URL, Item Barcode, and APIKEY
-        console.log(
-          "****************************************************",
-          data,
-          "******************************************************",
-        );
-        const dataObj = data;
-        //let resolvedData = await Promise.all(data);
-        // //dataObj.item_data.internal_note_3 = " ";
-        dataObj.item_data.barcode = item["Scanned Barcode"];
-        // //dataObj.item_data.barcode = item.Barcode;
-        const info = await axios.put(
-          process.env.DEV_EXLIBRIS_API_ROOT +
-            "/almaws/v1/bibs/" +
-            dataObj.bib_data.mms_id +
-            "/holdings/" +
-            dataObj.holding_data.holding_id +
-            "/items/" +
-            dataObj.item_data.pid +
-            "?apikey=" +
-            process.env.DEV_EXLIBRIS_API_BIB_UPDATE_KEY,
-          dataObj,
-        );
-        //console.log("data2 --------", info.data);
-      } catch (error) {
-        let barcode = error.config.url;
-        barcode = barcode.substr(69, 30);
-        const barcodeIndex = barcode.indexOf("&api");
-        barcode = barcode.slice(0, barcodeIndex);
 
-        console.log("barcode -- ", barcode, `\n`);
-
-        await fs
-          .createWriteStream("./errorLog.csv", { flags: "a" })
-          .write(`${barcode}, ${error.message}, Loop, \n`);
-
-        console.error(
-          "#######################################################################################################################################################",
-          error.config.url,
-          `\n`,
-        );
-      }
+      const { data } = await axios.get(
+        process.env.DEV_EXLIBRIS_API_ROOT +
+          process.env.DEV_EXLIBRIS_API_PATH +
+          item.Barcode +
+          //item["Scanned Barcode"] +
+          "&apikey=" +
+          process.env.DEV_EXLIBRIS_API_BIB_GET_KEY +
+          "&expand=p_avail",
+      );
+      //console.log("data -----   ", data);
+      //item update query to Alma backend. API URL, Item Barcode, and APIKEY
+      console.log(
+        "****************************************************",
+        data,
+        "******************************************************",
+      );
     });
+    // -------- promise.all(dataObjs) --------
+
+    // const dataObj = data;
+    // //let resolvedData = await Promise.all(data);
+    // // //dataObj.item_data.internal_note_3 = " ";
+    // dataObj.item_data.barcode = item["Scanned Barcode"];
+    // // //dataObj.item_data.barcode = item.Barcode;
+    // const info = await axios.put(
+    //   process.env.DEV_EXLIBRIS_API_ROOT +
+    //     "/almaws/v1/bibs/" +
+    //     dataObj.bib_data.mms_id +
+    //     "/holdings/" +
+    //     dataObj.holding_data.holding_id +
+    //     "/items/" +
+    //     dataObj.item_data.pid +
+    //     "?apikey=" +
+    //     process.env.DEV_EXLIBRIS_API_BIB_UPDATE_KEY,
+    //   dataObj,
+    // );
+    // console.log("data2 --------", info.data);
   } catch (error) {
+    let barcode = error.config.url;
+    barcode = barcode.substr(69, 30);
+    const barcodeIndex = barcode.indexOf("&api");
+    barcode = barcode.slice(0, barcodeIndex);
+
+    console.log("barcode -- ", barcode, `\n`);
+
     await fs
       .createWriteStream("./errorLog.csv", { flags: "a" })
-      .write(`Barcode, ${error.message}, Main,  \n`);
+      .write(`${barcode}, ${error.message}, Loop, \n`);
 
-    console.error("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++", error);
+    console.error(
+      "#######################################################################################################################################################",
+      error.config.url,
+      `\n`,
+    );
   }
 
   console.log("Can you see me now?");
