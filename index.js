15b238606db37e474261ffbe0aace4aba1da098b
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-04-06 14:02:59 -0700

    trying different fixes for the looping errors.

--- index.js
+++ index.js
@@ -32,61 +32,68 @@ const axios = require("axios");
 
       //const barcode = req.body.barcode.text;
       //console.log("barcode: ", item.Barcode);
+      try {
+        const { data } = await axios.get(
+          process.env.DEV_EXLIBRIS_API_ROOT +
+            process.env.DEV_EXLIBRIS_API_PATH +
+            item.Barcode +
+            //item["Scanned Barcode"] +
+            "&apikey=" +
+            process.env.DEV_EXLIBRIS_API_BIB_GET_KEY +
+            "&expand=p_avail",
+        );
 
-      const { data } = await axios.get(
-        process.env.DEV_EXLIBRIS_API_ROOT +
-          process.env.DEV_EXLIBRIS_API_PATH +
-          item.Barcode +
-          //item["Scanned Barcode"] +
-          "&apikey=" +
-          process.env.DEV_EXLIBRIS_API_BIB_GET_KEY +
-          "&expand=p_avail",
-      );
-      //console.log("data -----   ", data);
-      //item update query to Alma backend. API URL, Item Barcode, and APIKEY
-      console.log(
-        "****************************************************",
-        data,
-        "******************************************************",
-      );
+        return data;
+        //console.log("data -----   ", data);
+        //item update query to Alma backend. API URL, Item Barcode, and APIKEY
+        // console.log(
+        //   "****************************************************",
+        //   data,
+        //   "******************************************************",
+        // );
+        // const dataObj = data;
+        // //let resolvedData = await Promise.all(data);
+        // // //dataObj.item_data.internal_note_3 = " ";
+        // dataObj.item_data.barcode = item["Scanned Barcode"];
+        // // //dataObj.item_data.barcode = item.Barcode;
+        // const info = await axios.put(
+        //   process.env.DEV_EXLIBRIS_API_ROOT +
+        //     "/almaws/v1/bibs/" +
+        //     dataObj.bib_data.mms_id +
+        //     "/holdings/" +
+        //     dataObj.holding_data.holding_id +
+        //     "/items/" +
+        //     dataObj.item_data.pid +
+        //     "?apikey=" +
+        //     process.env.DEV_EXLIBRIS_API_BIB_UPDATE_KEY,
+        //   dataObj,
+        // );
+        // console.log("data2 --------", info.data);
+      } catch (error) {
+        let barcode = error.config.url;
+        barcode = barcode.substr(69, 30);
+        const barcodeIndex = barcode.indexOf("&api");
+        barcode = barcode.slice(0, barcodeIndex);
+
+        console.log("barcode -- ", barcode, `\n`);
+
+        await fs
+          .createWriteStream("./errorLog.csv", { flags: "a" })
+          .write(`${barcode}, ${error.message}, Loop, \n`);
+
+        console.error(
+          "#######################################################################################################################################################",
+          error.config.url,
+          `\n`,
+        );
+      }
     });
-    // -------- promise.all(dataObjs) --------
-
-    // const dataObj = data;
-    // //let resolvedData = await Promise.all(data);
-    // // //dataObj.item_data.internal_note_3 = " ";
-    // dataObj.item_data.barcode = item["Scanned Barcode"];
-    // // //dataObj.item_data.barcode = item.Barcode;
-    // const info = await axios.put(
-    //   process.env.DEV_EXLIBRIS_API_ROOT +
-    //     "/almaws/v1/bibs/" +
-    //     dataObj.bib_data.mms_id +
-    //     "/holdings/" +
-    //     dataObj.holding_data.holding_id +
-    //     "/items/" +
-    //     dataObj.item_data.pid +
-    //     "?apikey=" +
-    //     process.env.DEV_EXLIBRIS_API_BIB_UPDATE_KEY,
-    //   dataObj,
-    // );
-    // console.log("data2 --------", info.data);
   } catch (error) {
-    let barcode = error.config.url;
-    barcode = barcode.substr(69, 30);
-    const barcodeIndex = barcode.indexOf("&api");
-    barcode = barcode.slice(0, barcodeIndex);
-
-    console.log("barcode -- ", barcode, `\n`);
-
     await fs
       .createWriteStream("./errorLog.csv", { flags: "a" })
-      .write(`${barcode}, ${error.message}, Loop, \n`);
+      .write(`Barcode, ${error.message}, Main,  \n`);
 
-    console.error(
-      "#######################################################################################################################################################",
-      error.config.url,
-      `\n`,
-    );
+    console.error("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++", error);
   }
 
   console.log("Can you see me now?");
